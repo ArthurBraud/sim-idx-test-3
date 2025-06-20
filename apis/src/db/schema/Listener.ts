459ae15dbe0e1c75749a0323be966c497c282658
ArthurBraud <arthur@dune.com>
2025-06-20 17:06:05 +0200

    test

---
+++ apis/src/db/schema/Listener.ts
@@ -0,0 +1,14 @@
+
+import { pgEnum, pgTable as table } from "drizzle-orm/pg-core";
+import * as t from "drizzle-orm/pg-core";
+import * as simDb from "../../sim/db";
+import * as simTypes from "../../sim/types";
+
+export const poolCreated = table("pool_created", {
+  chainId: simDb.uint64('chain_id'),
+  caller: simDb.address('caller'),
+  pool: simDb.address('pool'),
+  token0: simDb.address('token0'),
+  token1: simDb.address('token1'),
+  fee: simDb.uint24('fee'),
+})
